afdafcf19b6db18bfd6dd9a28864d7d95125b42a
Evaristekanova <dusingizimanaevariste3@gmail.com>
2023-08-30 22:34:13 +0200

    cache data for increasing the parformance of the API

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -3,4 +3,5 @@ class OrderSerializer
   belongs_to :user
   has_many :products
   attributes 
+  cashe_options enabled: true, cache_length: 12.hours
 end
--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -2,4 +2,5 @@ class ProductSerializer
   include FastJsonapi::ObjectSerializer
   attributes :tittle, :price, :published
   belongs_to :user
+  cache_options enabled: true, cache_length: 12.hours
 end
