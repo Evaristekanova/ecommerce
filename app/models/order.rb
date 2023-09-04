4853e2838eac269221b7a4d43d0bb6ff4f65e730
Evaristekanova <dusingizimanaevariste3@gmail.com>
2023-09-04 23:58:03 +0200

    testing user controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   include ActiveModel::Validations
-  before_validatian :set_total!
+  before_validation :set_total!
   validate_with EnoughProductsValidator
   belongs_to :user
   validates :total, numericality: { greater_than_or_equal_to: 0 }
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create user" do
+    assert_difference('User.count') do
+    post api_v1_users_url, params: { user: { email:'test@test.org', password: '123456' } }, as: :json
+    end
+    assert_response :created
+  end
+
+  test "should not create user with invalid email" do
+    user = User.new(email: "text@1", password_digest: "123456")
+    assert_not user.valid?
+  end
+
+  test "should not create user with existing email" do
+    user = User.new(email: "one@one.gmail", password_digest: "123456")
+    assert_not user.valid?
+  end
 end
